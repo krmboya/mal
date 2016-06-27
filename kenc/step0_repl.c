d5a7353a98f60e3c9cef26e854e86506a1dc9306
Kenny Rachuonyo <krmboya@gmail.com>
2016-06-27 17:31:19 +0300

    step0 passes

--- kenc/step0_repl.c
+++ kenc/step0_repl.c
@@ -1,1 +1,34 @@
-int main() { return 0; }
+#include <stdio.h>
+
+char* read(char* x)
+{
+  return x;
+}
+
+char* eval(char* x)
+{
+  return x;
+}
+
+char* print(char* x)
+{
+  return x;
+}
+
+char* rep(char* x)
+{
+  return print(eval(read));
+}
+
+int main(int argc, char* argv[])
+{
+  char input[256];
+  while (1) {
+    fputs("user> ", stdout);
+    fgets(input, 256, stdin);
+    printf("%s", input);
+  }
+  return 0;
+}
+
+  
